d9e70e09927c0afe00e961390fd9a372b404ea86
PingxuHao <pvh5251@psu.edu>
2023-11-27 19:36:49 -0600

    remove trash files

--- streetlight/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-cseIP = parser.get('CONFIG_DATA', 'cseIP')
-csePort = parser.get('CONFIG_DATA', 'csePort')
-cseID = parser.get('CONFIG_DATA', 'cseID')
-cseName = parser.get('CONFIG_DATA', 'cseName')
